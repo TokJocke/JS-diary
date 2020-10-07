bacfe8fa66eeb3fbb4027553226122cfecc98034
Joakim <j-pf@live.se>
2020-10-07 16:01:17 +0200

    work work work.....

--- minDagBok.js
+++ minDagBok.js
@@ -1,13 +1,24 @@
 
 let myDiary = document.getElementById("diaryPost")
+let printRub = document.getElementById("printRub")
+let printDate = document.getElementById("printDate")
+let printPost = document.getElementById("printPost")
+
+
+
 
 
 function renderPost() {
 
-    let collectLocalPost = JSON.parse(localStorage.getItem("localPosts"))
+    let collectLocalPost = JSON.parse(localStorage.getItem("localPosts")) || [];
+
     console.log(collectLocalPost)
 
-    myDiary.innerText = collectLocalPost.newPost
+    printPost.innerText = collectLocalPost.newPost 
+    printDate.innerText = collectLocalPost.newDatum
+    printRub.innerText = collectLocalPost.newRubrik
+
+
 }  
 
 renderPost()
