9da7ab8f08b1f5a3ad6b123154ef5e2173e0d7be
Joakim <j-pf@live.se>
2020-10-07 14:59:05 +0200

    work in progress

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 
+
+let rubrik = document.getElementById("rubrik")
+let datum = document.getElementById("datum")
+let post = document.getElementById("post")
+let myDiary = document.getElementById("diaryPost")
+
+let submitButton = document.getElementById("submitButton")
+
+postArray = []
+
+let savedPost = {
+    newRubrik: undefined,    
+    newDatum: undefined,
+    newPost: undefined
+}
+
+
+submitButton.addEventListener("click", function(){
+
+savedPost.newRubrik = rubrik.value
+savedPost.newDatum = datum.value
+savedPost.newPost = post.value
+
+postArray.push("savedPost")
+
+ localStorage.setItem("localPosts", JSON.stringify(savedPost))
+ 
+
+/* console.log(savedPost)
+ */console.log(postArray)
+
+
+})
+
+function renderPost() {
+
+    let collectLocalPost = localStorage.getItem("localPosts")
+    console.log(collectLocalPost)
+
+    myDiary.appendChild(localPosts)
+}  
+
+renderPost()
